b033475518988f62f371bb5f140c6d39938a11a4
Léa <lea.lescure@hotmail.fr>
2020-11-15 22:40:31 +0100

    .h

---
+++ get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llescure <llescure@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/08 21:46:38 by llescure          #+#    #+#             */
+/*   Updated: 2020/11/15 22:17:27 by llescure         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 32
+#endif
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+
+int				get_next_line(int fd, char **line);
+size_t			ft_strlen(const char *str);
+char			*ft_strjoin(char const *s1, char const *s2);
+char			*ft_strchr(const char *s, int c);
+char			*ft_trim(char *str, int c);
+size_t			ft_strlcpy(char *dst, const char *src, size_t dstsize);
+
+#endif
