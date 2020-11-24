c2703dc44cbc0bdebb11b16199443bc0acf05daf
Léa <lea.lescure@hotmail.fr>
2020-11-24 23:31:36 +0100

    leaks to be fixed

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: llescure <llescure@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/08 21:46:38 by llescure          #+#    #+#             */
-/*   Updated: 2020/11/20 17:06:58 by llescure         ###   ########.fr       */
+/*   Updated: 2020/11/24 22:14:36 by llescure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 32
+#  define BUFFER_SIZE 10
 # endif
 
 # define BUF BUFFER_SIZE
@@ -29,7 +29,7 @@
 int				get_next_line(int fd, char **line);
 size_t			ft_strlen(const char *str);
 char			*ft_strjoin(char *s1, char *s2);
-char			*ft_strchr(const char *s, int c);
+char			*ft_memchr(const void *s, int c, size_t n);
 char			*ft_trim(char *str, int c);
 size_t			ft_strlcpy(char *dst, const char *src, size_t dstsize);
 
