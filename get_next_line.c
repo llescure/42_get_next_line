e648b947e4aac750b42e0afe1aefe181e2e7e3f8
Lea Lescure <lea.lescure@hotmail.fr>
2021-01-05 12:19:51 +0100

    fix fd issue

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: llescure <llescure@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/09 21:49:03 by llescure          #+#    #+#             */
-/*   Updated: 2020/12/03 23:10:23 by llescure         ###   ########.fr       */
+/*   Updated: 2021/01/05 12:07:22 by llescure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	get_next_line(int fd, char **line)
 	int					char_read;
 
 	char_read = 1;
-	if (fd < 0 || BUF <= 0 || line == NULL)
+	if (fd < 0 || BUF <= 0 || line == NULL || read(fd, 0, 0) < 0)
 		return (-1);
 	while ((ft_strchr(temp, '\n') == NULL) &&
 			(char_read = read(fd, buf, BUF)) > 0)
